95e4845a30ef65ab27a10751989dbb82d64b09b0
stefan <stefan.holmberg@systementor.se>
2024-12-10 15:25:14 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,93 @@
+function getBestPlayer(teamName, year){
+    if (year >2002 ){
+        return "Foppa";
+    }
+    return "Anders Eldebrink";
+}
+
+
+let res = getBestPlayer("312312", 1111)
+console.log(res)
+
+
+
+
+// isCool = confirm("Är du cool?")
+
+// namn = prompt("Vad heter du?")
+// if (namn === "Stefan"){
+//     alert("Fint namn")
+// }else{
+//     alert("Inte så fint men det duger väl")
+// }
+
+
+
+// // let player = {
+// //     name: "Foppa",
+// //     jersey:21
+// // }
+
+// // console.log(player.name)
+
+// // player.team = "Colorado"
+
+
+
+// // let x=12;
+
+// // //                  == betyder är det värde - JS typekonverterar om det är så
+// // if(x === 12){  // === betyuder är det samma datatyp och värde
+// //     console.log("Japp")
+// // }else{
+// //     console.log("Nope")
+// // }
+
+// // // console.log(x == 12);
+// // // console.log(x == "12");
+// // // console.log(x === "12" );
+// // // console.log(x === 12);
+
+
+// // // if( 0.1 + 0.2 == 0.3){  // == använder vi inte - vi använder istället ===
+// // //     console.log('Same')
+// // // }
+// // // else{
+// // //     console.log('Not same')
+// // // }
+    
+
+
+// // // // var player = new HockeyPlayer();
+// // // // HockeyPlayer player = new HockeyPlayer();
+
+
+// // // //var a = 12; // ANVÄND INTE !!!!
+// // // let a = 12; // KEYWORD let = best practice
+
+// // // const bestPlayerJerseyNumber = 21
+// // // let b = 12
+
+// // // let forNamn = "Stefan"
+
+// // // let efterNamn = 'Holmberg'
+
+// // // efterNamn = 12
+
+// // // let varde = "12"
+// // // let i = parseInt(varde)
+
+
+
+
+
+
+// // // console.log('Hejsan nu tar vi rast säger ' + forNamn + " RAST TILL 14:15")
+
+
+
+
+
+
+
+
